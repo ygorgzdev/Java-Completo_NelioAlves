2693ae5a52ca72b0f626a2d66ebc0deeb998de5a
ygorgzdev <ygorjosue7@gmail.com>
2025-11-03 09:20:10 -0300

    Output

--- curso-java/src/Exercicio.java
+++ curso-java/src/Exercicio.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Exercicio {
     public static void main(String[] args) {
         String product1 = "Computer";
@@ -11,9 +13,28 @@ public class Exercicio {
         double price2 = 650.50;
         double measure = 53.234567;
 
-        System.out.printf("Products:\n %s, which price is $ %f\n %s, which price is $ %f \n\n", product1, price1, product2, price2);
+        System.out.printf("Products:\n %s, which price is $ %.2f\n %s, which price is $ %.2f \n\n", product1, price1, product2, price2);
         System.out.printf("Record: %d years old, code %d and gender: %s \n\n ", age, code, gender);
+        System.out.printf("Measure with eight decimal places: %.8f\n Rouded (three decimal places): %.3f\n", measure, measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f\n\n", measure);
+
+        // Outra forma, resolvida Nélio Alves
+
+        // Acertei no geral, mas a formatação do texto poderia ser melhor, encurtei de mais
+
+        // Quando quisermos pular uma linha para imprimir, melhor usar um \n e um sout() vazio, mais arrumadinho!
+
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+        System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+        System.out.println();
+        System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
+        System.out.println();
+        System.out.printf("Measue with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f%n", measure);
 
     }
-}
 }
