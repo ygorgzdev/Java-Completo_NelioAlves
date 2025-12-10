fb428fca085fa96b30654b0c4d85f80a5a56525d
ygorgzdev <ygorjosue7@gmail.com>
2025-12-10 08:47:38 -0300

    Exercicios

--- curso-java/src/Exercicios/ER/Exercicio1.java
+++ curso-java/src/Exercicios/ER/Exercicio1.java
@@ -1,4 +1,30 @@
 package Exercicios.ER;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Exercicio1 {
+
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Digite a senha da porta: ");
+        int senha = ler.nextInt();
+
+        while ( senha != 2002) {
+            System.out.println();
+            System.out.println("Senha inválida!");
+            System.out.println();
+            System.out.print("Digite novamente: ");
+            senha = ler.nextInt();
+        }
+
+        System.out.println();
+        System.out.println("Acesso Permitido!");
+
+        ler.close();
+
+    }
+
+
 }
